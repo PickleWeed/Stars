8d912e8a6cc2810f4e4498720f6e3d98bb48d7ca
ece2727 <69996862+ece2727@users.noreply.github.com>
2020-11-21 05:49:53 +0800

    Revert "Done vacancy for student"
    This reverts commit 86610b1f7c4e3748998a9cbd653fef6d38c502e7.

---
+++ MySTARS/src/Student/CheckVacancies.java
@@ -0,0 +1,12 @@
+package Student;
+
+/**
+ * 
+ * Check Vacancies Available is a control class that displays the number of remaining
+ * vacancies of a course selected by the user.
+ * @author ?
+ *
+ */
+public class CheckVacancies {
+
+}
