76a3e310090ff52c01dc9af7a59627c6fd4bf479
lihaoyangML <60748402+lihaoyangML@users.noreply.github.com>
2020-11-21 00:34:58 +0800

    Update ChangeIndex.java

--- MySTARS/src/Student/ChangeIndex.java
+++ MySTARS/src/Student/ChangeIndex.java
@@ -1,5 +1,12 @@
 package Student;
 
+/**
+ * ChangeIndex is a control class that allows the user to changes the index of a course
+ * to another index of the same course.
+ * 
+ * @author ?
+ *
+ */
 public class ChangeIndex {
 
 }
