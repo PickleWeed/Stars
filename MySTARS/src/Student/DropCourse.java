03435d65a6417b834326fcd0d237b92bebc7009a
lihaoyangML <60748402+lihaoyangML@users.noreply.github.com>
2020-11-21 00:38:22 +0800

    Update DropCourse.java

--- MySTARS/src/Student/DropCourse.java
+++ MySTARS/src/Student/DropCourse.java
@@ -6,7 +6,18 @@ import java.nio.file.Paths;
 import java.util.Scanner;
 import java.util.stream.Stream;
 
+/**
+ * DropCourse is a control class which implements the logic of dropping a course for 
+ * the student if the student enters a valid courseIndex.
+ * 
+ * @author ?
+ *
+ */
 public class DropCourse {
+	/**
+	 * prompts the user to enter the courseIndex to drop.
+	 * @return courseIndex to be dropped
+	 */
 	public String queryCourseIndex() {
 		String courseIndex;
 		Scanner sc = new Scanner(System.in);
@@ -14,6 +25,14 @@ public class DropCourse {
 	    courseIndex = sc.nextLine();
 	    return courseIndex;
 	}
+	
+	/**
+	 * Checks if the courseIndex entered by the user is a valid courseIndex in the database,
+     	 * and returns either a valid courseIndex, or "".
+	 * @param courseIndex courseIndex (eg. CZ1003)
+	 * @return if a matching courseIndex is found in the database, return the courseIndex.
+	 * Otherwise, return "".
+	 */
     public String findString(String courseIndex){
     	String data, Str, subData, temp;
     	try {
@@ -35,6 +54,12 @@ public class DropCourse {
     	}
         return "";
     }
+	
+    	/**
+     	 * Drops the course for the student if the courseIndex is valid.
+	 * @param fileLine fileLine is either a valid courseIndex(one that is present in the database), 
+	 * or "". 
+	 */
    public void dropCourse(String fileLine) {
    		try { 
 	       	File fileName = new File("StudentRecords.txt");
