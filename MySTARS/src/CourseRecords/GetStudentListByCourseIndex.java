a0e1b8610dac72cfa1539f88e57ffe476855bd10
lihaoyangML <60748402+lihaoyangML@users.noreply.github.com>
2020-11-21 12:51:16 +0800

    Update GetStudentListByCourseIndex.java

--- MySTARS/src/CourseRecords/GetStudentListByCourseIndex.java
+++ MySTARS/src/CourseRecords/GetStudentListByCourseIndex.java
@@ -6,10 +6,23 @@ import java.util.ArrayList;
 import StudentInfo.Student;
 import StudentRecords.StudentRecords;
 
-
-
+/**
+ * GetStudentListByCourseIndex is a boundary class which allows a user to select an CourseIndex (eg. CZ1003)
+ * from a list of all possible CourseIndex and displays all students who have registered for a course 
+ * under this CourseIndex.
+ * 
+ * @author ?
+ *
+ */
 public class GetStudentListByCourseIndex {
 
+	/**
+	 * Displays all possible CourseIndex
+	 * 
+	 * @param StudentRecordsList Entire database of StudentRecords
+	 * @param studentInfo Entire database of StudentInfo
+	 * @throws IOException If an input or output exception occurred
+	 */
 	public static void showcourseIndex(ArrayList StudentRecordsList,ArrayList studentInfo) throws IOException
 	{
 		System.out.println("List of Index Number:");
@@ -23,6 +36,14 @@ public class GetStudentListByCourseIndex {
 				System.out.println("Index Number: " + studentRecords.getCourseIndex());
 		}
 	}
+	
+	/**
+	 * Displays all students who have registered a course with the CourseIndex
+	 * 
+	 * @param courseIndex all students who have registered a course with this CourseIndex will be displayed
+	 * @param StudentRecordsList Entire database of StudentRecords
+	 * @param studentInfo Entire database of StudentInfo
+	 */
 	public static void getKey(String courseIndex, ArrayList StudentRecordsList,ArrayList studentInfo)
 	{
 		for (int i=0; i<StudentRecordsList.size(); i++)
