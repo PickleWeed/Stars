f23f65b39826207715c06c85efeb604c12f32ed3
lihaoyangML <60748402+lihaoyangML@users.noreply.github.com>
2020-11-23 17:34:50 +0800

    Update UpdateStudentRecord.java

--- MySTARS/src/StudentRecords/UpdateStudentRecord.java
+++ MySTARS/src/StudentRecords/UpdateStudentRecord.java
@@ -9,7 +9,7 @@ public class UpdateStudentRecord {
 		for (int i = 0 ; i < StudentRecord.size() ; i++) {
 			StudentRecords studentRecord = (StudentRecords)StudentRecord.get(i);
 			
-			if(studentRecord.getIndexNum().equals(indexNum) && studentRecord.getCourseIndex().equals(StudentRecord))
+			if(studentRecord.getIndexNum().equals(indexNum) && studentRecord.getCourseIndex().equals(courseIndex))
 			{
 				System.out.println("Old AU: " + studentRecord.getAU() + ", New AU: " + AU);
 				studentRecord.setAU(AU);
