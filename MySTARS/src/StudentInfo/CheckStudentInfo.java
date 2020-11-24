89b69605ed3dab6629a1d71053d732386ce51474
lihaoyangML <60748402+lihaoyangML@users.noreply.github.com>
2020-11-24 12:04:53 +0800

    Update CheckStudentInfo.java

--- MySTARS/src/StudentInfo/CheckStudentInfo.java
+++ MySTARS/src/StudentInfo/CheckStudentInfo.java
@@ -4,7 +4,23 @@ import java.util.ArrayList;
 
 import Login.LoginInfo;
 
+/**
+ * CheckStudentInfo checks if given matricNum/username/email already exits in the database.
+ * Its methods will return true if this happens, and print a message to inform the user.
+ * Otherwise, it will return false.
+ * 
+ * @author ?
+ *
+ */
 public class CheckStudentInfo {
+	
+	/**
+	 * Checks if given matricNum already exits in the database.
+	 * 
+	 * @param matricNum matriculation number
+	 * @param studentInfo An ArrayList of all students' information in the studentInfo database
+	 * @return true if the given matricNum already exits in the database, false if otherwise
+	 */
 	public static boolean checkMatricNum(String matricNum, ArrayList studentInfo)
 	{
 		for(int i=0; i<studentInfo.size(); i++)
@@ -18,6 +34,14 @@ public class CheckStudentInfo {
 		}
 		return false;
 	}
+	
+	/**
+	 * Checks if given username already exits in the database.
+	 * 
+	 * @param username username of the student
+	 * @param LoginArray An ArrayList of all students' login information in the LoginsInfo database
+	 * @return true if the given user already exits in the database, false if otherwise
+	 */
 	public static boolean checkUsername(String username, ArrayList LoginArray)
 	{
 		for(int i=0; i<LoginArray.size(); i++)
@@ -32,6 +56,14 @@ public class CheckStudentInfo {
 		return false;
 		
 	}
+	
+	/**
+	 * Checks if given email already exits in the database.
+	 * 
+	 * @param email the email of the student
+	 * @param studentInfo An ArrayList of all students' information in the studentInfo database
+	 * @return true if the given email already exits in the database, false if otherwise
+	 */
 	public static boolean checkEmail(String email, ArrayList studentInfo)
 	{
 		for(int i=0; i<studentInfo.size(); i++)
