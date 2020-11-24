bc6164088cd95e3203da9fe2bae588a191d4fba1
lihaoyangML <60748402+lihaoyangML@users.noreply.github.com>
2020-11-24 12:10:25 +0800

    Update GetStudentInfo.java

--- MySTARS/src/StudentInfo/GetStudentInfo.java
+++ MySTARS/src/StudentInfo/GetStudentInfo.java
@@ -2,8 +2,21 @@ package StudentInfo;
 
 import java.util.ArrayList;
 
-
+/**
+ * GetStudentInfo displays student informations based on a given matriculation number.
+ * 
+ * @author ?
+ *
+ */
 public class GetStudentInfo {
+	
+	/**
+	 * displays all informations of student/students in the database with the same 
+	 * key that is passed into the method.
+	 * 
+	 * @param studentInfo An Arraylist of all studentInfo in the database
+	 * @param key The key of the student
+	 */
 	public static void printStudentInfo(ArrayList studentInfo, String key)
 	{
 		for (int i = 0 ; i < studentInfo.size() ; i++) {
@@ -21,6 +34,15 @@ public class GetStudentInfo {
 				}
 		}
 	}
+	
+	/**
+	 * finds the key of the student in the database with the same matriculation number that is passed
+	 * into the method.
+	 * 
+	 * @param studentInfo An Arraylist of all studentInfo in the database
+	 * @param matricNum matriculation number of the student
+	 * @return The key of the student, or nill if the matriculation number is not found in the database.
+	 */
 	public static String getStudentKey(ArrayList studentInfo, String matricNum)
 	{
 		//get key
