0522e6e431ed6c0b9b43feab3e4f70a9232a1f7c
lihaoyangML <60748402+lihaoyangML@users.noreply.github.com>
2020-11-24 09:31:29 +0800

    Update CheckCourseRecord.java

--- MySTARS/src/CourseRecords/CheckCourseRecord.java
+++ MySTARS/src/CourseRecords/CheckCourseRecord.java
@@ -2,7 +2,23 @@ package CourseRecords;
 
 import java.util.ArrayList;
 
+/**
+ * CheckCourseRecord searches the existence of an entry in the CourseRecords
+ * database based on an query(partial informations of a course).
+ * 
+ * @author ?
+ *
+ */
 public class CheckCourseRecord {
+	
+	/**
+	 * Checks if a course with the same courseIndex and indexNum is currently in the database.
+	 * 
+	 * @param courseRecord An ArrayList of all course informations in the CourseRecords database
+	 * @param courseIndex courseIndex to be checked
+	 * @param indexNum indexNum to be checked
+	 * @return true if a matching courseIndex and indexNum pair is found in the database, false otherwise.
+	 */
 	public static boolean checkCourse(ArrayList courseRecord, String courseIndex, String indexNum)
 	{
 		for (int i = 0 ; i < courseRecord.size() ; i++) {
@@ -13,6 +29,20 @@ public class CheckCourseRecord {
 		System.out.println("Invalid Course Index & Index Number\n");
 		return false;
 	}
+	
+	/**
+	 * Checks if a course with the same courseIndex, indexNum and num is currently 
+	 * in the database.
+	 * 
+	 * @param courseRecord An ArrayList of all course informations in the CourseRecords database
+	 * @param num a number useful for identifying a courseIndex and index pair (eg. CZ1003|10088) 
+	 * when multiple same courseIndex and index pairs are present. <p> For example, num = 1
+	 * for CZ1003|10088|LAB and num = 2 for CZ1003|10088|TUT
+	 * @param courseIndex courseIndex to be checked
+	 * @param indexNum indexNum indexNum to be checked
+	 * @return true if a matching entry with the same num, courseIndex and indexNum 
+	 * is found in the database, false otherwise.
+	 */
 	public static boolean checkNum(ArrayList courseRecord, String num, String courseIndex, String indexNum)
 	{
 		for (int i = 0 ; i < courseRecord.size() ; i++) {
@@ -23,6 +53,15 @@ public class CheckCourseRecord {
 		System.out.println("Invalid No. \n");
 		return false;
 	}
+	
+	/**
+	 * Checks if a course to be added into the system already exists in the system.
+	 * 
+	 * @param courseRecord An ArrayList of all course informations in the CourseRecords database
+	 * @param courseIndex courseIndex to be checked
+	 * @param indexNum indexNum to be checked
+	 * @return true if the course to be added already exists, false otherwise
+	 */
 	public static boolean checkDuplicate(ArrayList courseRecord, String courseIndex, String indexNum)
 	{
 		for (int i = 0 ; i < courseRecord.size() ; i++) {
