6e2eefaf35ae23682adb24372261212ec1427e6d
lihaoyangML <60748402+lihaoyangML@users.noreply.github.com>
2020-11-24 09:47:35 +0800

    Update GetCourseRecord.java

--- MySTARS/src/CourseRecords/GetCourseRecord.java
+++ MySTARS/src/CourseRecords/GetCourseRecord.java
@@ -3,7 +3,22 @@ package CourseRecords;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * GetCourseRecord contains multiple options to display
+ * information of courses in the CourseRecords database.
+ * 
+ * @author ?
+ *
+ */
 public class GetCourseRecord {
+	
+	/**
+	 * Gets the list of all distinct indexNum in the database.
+	 * 
+	 * @param courseRecord An ArrayList of all course informations in the CourseRecords database
+	 * @throws IOException If an input or output exception occurred
+	 * @return list of all distinct indexNum in the database
+	 */
 	public static ArrayList getIndexList(ArrayList courseRecord) throws IOException
 	{
 		ArrayList<String> indexList = new ArrayList<String>();
@@ -16,6 +31,13 @@ public class GetCourseRecord {
 		}
 		return indexList;
 	}
+	
+	/**
+	 * Displays indexNum of all courses in the database.
+	 * 
+	 * @param courseRecord An ArrayList of all course informations in the CourseRecords database
+	 * @throws IOException If an input or output exception occurred
+	 */
 	public static void printIndexNum(ArrayList courseRecord) throws IOException
 	{
 		System.out.println("List of Index Number:");
@@ -27,6 +49,13 @@ public class GetCourseRecord {
 				System.out.println("Index Number: " + courseRecords.getIndexNum() + ", Name: " + courseRecords.getName());
 		}
 	}
+	
+	/**
+	 * Displays the vacancy and other course informations for courses with a particular indexNum.
+	 * 
+	 * @param indexNum The indexNum used to selectively displays the vacancies left courses 
+	 * @param courseRecord An ArrayList of all course informations in the CourseRecords database
+	 */
 	public static void printVacancy(String indexNum, ArrayList courseRecord)
 	{
 //		for (int i=0; i<courseRecord.size(); i++)
@@ -63,6 +92,12 @@ public class GetCourseRecord {
 		}
 	}
 	
+	/**
+	 * Displays all informations of all courses in the CourseRecords database.
+	 * 
+	 * @param courseRecord An ArrayList of all course informations in the CourseRecords database
+	 * @throws IOException If an input or output exception occurred
+	 */
 	public static void printAllCourseData(ArrayList courseRecord) throws IOException
 	{
 		for (int i = 0 ; i < courseRecord.size() ; i++) {
@@ -84,6 +119,15 @@ public class GetCourseRecord {
 				System.out.println("");
 			}
 	}
+	
+	/**
+	 * Displays all informations of courses with matching courseIndex and indexNum
+	 * in the CourseRecords database.
+	 * 
+	 * @param courseRecord An ArrayList of all course informations in the CourseRecords database
+	 * @param courseIndex The courseIndex used to extract course information
+	 * @param indexNum The IndexNum useds to extract course information
+	 */
 	public static void printCourseCode(ArrayList courseRecord, String courseIndex, String indexNum)
 	{
 		for (int i = 0 ; i < courseRecord.size() ; i++) {
@@ -110,6 +154,13 @@ public class GetCourseRecord {
 
 		}
 	}
+	
+	/**
+	 * Displays the courseIndex and indexNum of all courses in the CourseRecords database.
+	 * 
+	 * @param courseRecord An ArrayList of all course informations in the CourseRecords database
+	 * @throws IOException If an input or output exception occurred
+	 */
 	public static void printCourseCode(ArrayList courseRecord) throws IOException
 	{
 		for (int i = 0 ; i < courseRecord.size() ; i++) {
