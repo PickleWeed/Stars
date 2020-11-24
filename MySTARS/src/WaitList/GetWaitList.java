29f43c100876464bab132af4fbb71b47412e2f4b
lihaoyangML <60748402+lihaoyangML@users.noreply.github.com>
2020-11-24 21:56:26 +0800

    Update GetWaitList.java

--- MySTARS/src/WaitList/GetWaitList.java
+++ MySTARS/src/WaitList/GetWaitList.java
@@ -2,7 +2,22 @@ package WaitList;
 
 import java.util.ArrayList;
 
+/**
+ * GetWaitList performs query on the WaitList database
+ * 
+ * @author Aaron
+ *
+ */
 public class GetWaitList {
+	/**
+	 * gets the number of people waiting for the particular course and index
+	 * 
+	 * @param waitList the waitList database
+	 * @param courseIndex the courseIndex to be checked
+	 * @param indexNum the indexNum to be checked
+	 * @param queue queue
+	 * @return the number of people waiting for the particular course and index
+	 */
 	public static String getQueue(ArrayList waitList, String courseIndex, String indexNum, String queue)
 	{
 		for (int i = 0 ; i < waitList.size() ; i++) {
@@ -12,6 +27,15 @@ public class GetWaitList {
 		}
 		return null;
 	}
+	
+	/**
+	 * Checks if a course has a queue (1 or more people on the waiting list)
+	 * 
+	 * @param waitList the waitList database
+	 * @param courseIndex the courseIndex to be checked
+	 * @param indexNum the indexNum to be checked
+	 * @return false if there is no queue for this course, true otherwise
+	 */
 	public static boolean hasQueue(ArrayList waitList, String courseIndex, String indexNum)
 	{
 		for (int i = 0 ; i < waitList.size() ; i++) {
